4f7fe3acb022dfb6d59368a97109ae93a2b47ccc
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2022-10-28 16:33:48 -0500

    delete

--- application/views/admin/template.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('admin/layout/header'); ?>
-<?php $this->load->view('admin/layout/sidebar'); ?>
-<?=$contents?>
-<?php $this->load->view('admin/layout/footer'); ?>
- 
--- application/views/be/template.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('be/layout/header'); ?>
-<?php $this->load->view('be/layout/sidebar'); ?>
-<?=$contents?>
-<?php $this->load->view('be/layout/footer'); ?>
- 
